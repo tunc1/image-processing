1a802a61b951df55dcdaa81d5cd867b423671bd4
User <user>
2021-03-27 00:05:53 +0300

    Added bitmap

--- image_processing.py
+++ image_processing.py
@@ -12,3 +12,16 @@ def grayscale(array):
             tmp.append(mean)
         new_array.append(tmp)
     return numpy.array(new_array)
+
+def bitmap(array,threshold):
+    new_array=[]
+    for rows in array:
+        tmp=[]
+        for pixel in rows:
+            mean=0
+            for color in pixel:
+                mean+=color
+            mean/=3
+            tmp.append(255 if mean>threshold else 0)
+        new_array.append(tmp)
+    return numpy.array(new_array)
