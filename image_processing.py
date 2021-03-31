59cfafa7bf859475e0e092bbf47f8b322a6e9f7b
User <user>
2021-04-01 00:27:18 +0300

    Added Prewitt

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 
 def grayscale(image):
     """
@@ -230,3 +231,25 @@ def custom(image):
     Takes in a grayscale or bitmap image (NumPy array), applies custom, returns image
     """
     return __apply_3x3_kernel(image,numpy.array([[0,0,0],[0,1,0],[0,0,0]]))
+
+def prewitt(image):
+    """
+    Takes in a grayscale or bitmap image (NumPy array), applies prewitt, returns image
+    """
+    kernely=numpy.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+    kernelx=numpy.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+    new_image=[]
+    for y in range(1,len(image)-1):
+        new_row=[]
+        for x in range(1,len(image[0])-1):
+            tmp=numpy.array(image[y-1:y+2,x-1:x+2])
+            resultx=(tmp*kernelx).sum()
+            resulty=(tmp*kernely).sum()
+            result=math.sqrt(math.pow(resultx,2)+math.pow(resulty,2))
+            if result>255:
+                result=255
+            elif result<0:
+                result=0
+            new_row.append(result)
+        new_image.append(new_row)
+    return numpy.array(new_image)
