1634f200730cbbb6367fc2ef848b392dc0ab0fee
user <user>
2021-04-28 19:37:54 +0300

    Add decrease_each_pixel test

--- test.py
+++ test.py
@@ -21,3 +21,9 @@ class ImageProcessingTest(TestCase):
         expected=numpy.array([[[94,181,37],[255,189,66]],[[42,213,255],[78,132,222]]])
         output=image_processing.increase_each_pixel(input,30)
         self.assertTrue(numpy.array_equal(output,expected))
+
+    def test_decrease_each_pixel(self):
+        input=numpy.array([[[64,151,7],[231,159,36]],[[12,183,246],[48,102,192]]])
+        expected=numpy.array([[[34,121,0],[201,129,6]],[[0,153,216],[18,72,162]]])
+        output=image_processing.decrease_each_pixel(input,30)
+        self.assertTrue(numpy.array_equal(output,expected))
