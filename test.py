35df34875ed14cd3713062f576f311aea7634dc0
user <user>
2021-04-28 19:34:35 +0300

    Add bitmap test

--- test.py
+++ test.py
@@ -9,3 +9,9 @@ class ImageProcessingTest(TestCase):
         expected=numpy.array([[164,142],[147,144]])
         output=image_processing.grayscale(input)
         self.assertTrue(numpy.array_equal(output,expected))
+
+    def test_bitmap(self):
+        input=numpy.array([[[64,151,7],[231,159,36]],[[12,183,246],[48,102,192]]])
+        expected=numpy.array([[0,255],[255,0]])
+        output=image_processing.bitmap(input)
+        self.assertTrue(numpy.array_equal(output,expected))
