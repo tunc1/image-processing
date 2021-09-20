f5ed7a7e9d0d8fac78833cb0bee7b5536dda2011
user <email>
2021-09-20 19:45:02 +0300

    Add increaseEachPixel

--- src/app/ImageProcessing.java
+++ src/app/ImageProcessing.java
@@ -19,6 +19,28 @@ public class ImageProcessing
         }
         return image;
     }
+    public BufferedImage increaseEachPixel(BufferedImage image,int value)
+    {
+        for(int y=0;y<image.getHeight();y++)
+        {
+            for(int x=0;x<image.getWidth();x++)
+            {
+                Color pixel=new Color(image.getRGB(x,y),true);
+                int[] colors={pixel.getRed(),pixel.getGreen(),pixel.getBlue()};
+                for(int i=0;i<colors.length;i++)
+                {
+                    int color=colors[i]+value;
+                    if(color>255)
+                        colors[i]=255;
+                    else if(color<0)
+                        colors[i]=0;
+                }
+                Color newColor=new Color(colors[0],colors[1],colors[2]);
+                image.setRGB(x,y,newColor.getRGB());
+            }
+        }
+        return image;
+    }
     public BufferedImage bitmap(BufferedImage image,int threshold)
     {
         for(int y=0;y<image.getHeight();y++)
